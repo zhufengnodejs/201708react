038d4a82d1fd09ea468ac6bda14e64fcf09a0f72
zhufengnodejs <zhufengnodejs@126.com>
2017-10-31 11:56:37 +0800

    jsx

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-//react是核心库
 import React from 'react';
-//react跟DOM相关的库
 import ReactDOM from 'react-dom';
-//1.使用ReactDOM的render方法，把一个h1标题存放在DOM容器内容
-//2.因为这种JSX语法是React扩展的语义，跟原生JS不一样。这样的代码是不是能直接在浏览器中运行的，需要经过webpack中babel-loader的编译
-//3.React元素是构建React应用的基本单位
-//4.如果希望在React元素内嵌表达式，则需要用{}包裹起来。表达式就是任意的变量、数字的运算结合.表达必须要返回一个结果。
-let user = {
-  firstName:'王',
-  lastName:'肖肖'
-}
-//Objects are not valid as a React child
-//对象不是React的合法子元素
-function format(user){
-  return `${user.firstName}${user.lastName}`;
-}
-let element = <h1>{format(user)}</h1>;
+//JSX的属性
+//tabIndex 指的是按tab按切换焦点时候的索引
+let imgSrc = 'https://www.baidu.com/img/wanshengdoodle_677234cad70a5974a64e4665c6485c71.gif';
+//当给React元素添加属性的时候，属性名称跟原生的HTML不一样。要用驼峰命名法。
+//有些特殊的属性比如class=>className for=>htmlFor
+//如果给行内样式的话字符串的话直接给。
+//The `style` prop expects a mapping from style properties to values, not a string
+//style属性期待一个样式对象的映射，而非字符串
+let ele = <div>
+  <button tabIndex="1">1</button>
+  <button tabIndex="2">2</button>
+  <button tabIndex="3">3</button>
+  <img
+    style={{border:'1px solid red'}}
+    src={imgSrc}
+    className="myimg"/>
+</div>;
+console.log(ele);
+//ele不是大名鼎鼎的虚拟DOM
+//其实是一个普通的JS对象，但是可以用来描述真实DOM长什么样子
+// type props (children)
 ReactDOM.render(
-  element,
+  ele,
   document.querySelector('#root')
 );
