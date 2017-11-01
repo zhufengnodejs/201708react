c034b00753aa6deaa7b88db80a731afc11b11eb3
zhufengnodejs <zhufengnodejs@126.com>
2017-11-01 17:51:44 +0800

    state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+//受控组件的意思是input显示的值受React控制
 class RegForm extends Component{
+  constructor(){
+    super();
+    this.state = {username:'',password:''};
+  }
+  handleUserName = (event)=>{
+    let username = event.target.value;//取得输入框的值
+    this.setState({username});
+  }
+  handlePassword = (event)=>{
+    let password = event.target.value;//取得输入框的值
+    this.setState({password});
+  }
   render(){
     return (
       <form>
         <label>用户名
-          <input type="text" />
+          <input
+                 type="text"
+                 onChange={this.handleUserName}
+                 value={this.state.username} />
         </label>
         <label>密码
-          <input type="text"/>
+          <input type="text"
+                 onChange={this.handlePassword}
+                 value={this.state.password}/>
         </label>
         <input type="submit"/>
       </form>
