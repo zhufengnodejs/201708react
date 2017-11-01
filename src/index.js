4061dc3b929e450127153f9f7df14b268f6117f8
zhufengnodejs <zhufengnodejs@126.com>
2017-11-01 15:36:05 +0800

    state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 function UserGreeting(){
   return <h1>欢迎回来</h1>;
 }
 function GuestGreet(){
-  return <h1>请注册</h1>;
+  return <h1>请登录</h1>;
 }
 function Greeting(props){
   let {isLogin} = props;
@@ -12,4 +12,27 @@ function Greeting(props){
     return <UserGreeting/>
   return <GuestGreet/>
 }
-ReactDOM.render(<Greeting isLogin={true}/>,document.querySelector('#root'));
+function LoginButton(props){
+  return <button onClick={props.onClick}>登录</button>
+}
+function LogoutButton(props){
+ return <button onClick={props.onClick}>退出</button>
+}
+class LoginCtrl extends Component{
+  constructor(){
+    super();
+    this.state = {isLogin:false};
+  }
+  render(){
+    let {isLogin} = this.state;
+    let button = isLogin?<LogoutButton onClick={()=>this.setState({isLogin:false})}/>
+      :<LoginButton  onClick={()=>this.setState({isLogin:true})}/>;
+    return (
+      <div>
+        <Greeting isLogin={this.state.isLogin}/>
+        {button}
+      </div>
+    )
+  }
+}
+ReactDOM.render(<LoginCtrl/>,document.querySelector('#root'));
