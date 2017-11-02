a439724a4c742ca0c51072b1f66b3731c105f714
zhufengnodejs <zhufengnodejs@126.com>
2017-11-02 17:49:46 +0800

    state

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 export default class ProductTable extends Component{
   render(){
     let rows = [];//React元素 虚拟DOM
+    let lastCategory;//保存上一个分类
     for(let i=0;i<this.props.products.length;i++){
       let product = this.props.products[i];//取出当前商品
       //如果有过滤条件，并且当前商品的名称不包含此过滤的关键字，过跳过这一个商品
@@ -10,6 +11,11 @@ export default class ProductTable extends Component{
       //如果只显示有存货的，并且当前商品没有存货的话，则也跳过当前商品
       if(this.props.inStockOnly && !product.stocked)
         continue;
+      if(!lastCategory || product.category!=lastCategory){
+        rows.push(<tr><td colSpan={2} style={{fontWeight:'bold'}}>{product.category}</td></tr>);
+        //把当前的分类当成上一个分类
+        lastCategory = product.category;
+      }
       //如果上述条件都满足的话，则把当前商品变成一个行并添加行数组里
       rows.push(<tr key={i}><td style={{color:product.stocked?'black':'red'}}>{product.name}</td>
         <td>{product.price}</td></tr>);
