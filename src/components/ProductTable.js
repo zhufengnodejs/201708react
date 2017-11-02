ed63cd4f83e36394b1857cfa0bb4d448d932458f
zhufengnodejs <zhufengnodejs@126.com>
2017-11-02 17:21:45 +0800

    state

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,10 +1,26 @@
 import React,{Component} from 'react';
 export default class ProductTable extends Component{
   render(){
+    let rows = [];
+    for(let i=0;i<this.props.products.length;i++){
+      let product = this.props.products[i];
+      rows.push(<tr key={i}><td>{product.name}</td>
+        <td>{product.price}</td></tr>);
+    }
     return (
-      <div>
-        ProductTable
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>商品名称</th>
+            <th>商品价格</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          rows
+        }
+        </tbody>
+      </table>
     )
   }
 }
