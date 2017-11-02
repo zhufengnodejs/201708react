f19fa199ce462cb26eb584688d44a9313d146936
zhufengnodejs <zhufengnodejs@126.com>
2017-11-02 16:57:46 +0800

    state

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -9,10 +9,22 @@ export default class FilterableProductTable extends Component {
       inStockOnly:false//只显示有存货的商品
     };
   }
+  handleFilterText = (event)=>{//处理搜索框的值改变事件
+    this.setState({
+      filterText:event.target.value
+    })
+  }
+  handleInStockOnly = (event)=>{//处理复选框的值改变事件
+   this.setState({
+     inStockOnly:event.target.checked
+   });
+  }
   render() {
     return (
       <div>
         <SearchBar
+          handleFilterText = {this.handleFilterText}
+          handleInStockOnly = {this.handleInStockOnly}
           filterText={this.state.filterText}
           inStockOnly={this.state.inStockOnly}
         />
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,8 +3,12 @@ export default class SearchBar extends Component{
   render(){
     return (
       <div>
-        <input type="text" value={this.props.filterText}/><br/>
-        <input type="checkbox" checked={this.props.inStockOnly}/>只显示有现货的商品
+        <input type="text"
+               onChange={this.props.handleFilterText}
+               value={this.props.filterText}/><br/>
+        <input type="checkbox"
+               onChange={this.props.handleInStockOnly}
+               checked={this.props.inStockOnly}/>只显示有现货的商品
       </div>
     )
   }
