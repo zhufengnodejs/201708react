c69a91cf903ca8655d5ff423ac1dd76ae84cf53f
zhufengnodejs <zhufengnodejs@126.com>
2017-11-07 18:06:10 +0800

    react面试题

--- src/routes/4.transition.js
+++ src/routes/4.transition.js
@@ -1,48 +1,54 @@
-import React, {Component} from 'react';
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import './4.transition.css'
-/**
- * 在一个组件进入或离开的时候添加一系列的类名
- * 比如在进行的时候 example-enter example-enter-active
- **/
-export default class Todos extends Component {
-  constructor() {
-    super();
-    this.state = {todos: ["a", "b"]};
-  }
+import React from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-  addTodo = (event) => {
-    let keyCode = event.keyCode;
-    if (keyCode == 13) {
-      let text = event.target.value;
-      this.setState({todos: [...this.state.todos, text]});
-      event.target.value = '';
-    }
-  }
+import './4.transition.css';
 
+const Fade = ({ children, ...props }) => (
+  <CSSTransition
+    {...props}
+    timeout={1000}
+    classNames="fade"
+  >
+    {children}
+  </CSSTransition>
+);
+
+
+export default class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { items: ['hello', 'world', 'click', 'me'] }
+  }
+  handleAdd() {
+    this.setState({
+      items: [
+        ...this.state.items,
+        prompt('Enter some text')
+      ]
+    });
+  }
+  handleRemove(i) {
+    let newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({ items: newItems });
+  }
   render() {
     return (
-      <div>
-        <input
-          onKeyDown={this.addTodo}
-          type="text"/><br/>
-
-        <TransitionGroup>
-          <ul>
-            {
-              this.state.todos.map((item, index) => (
-                <CSSTransition
-                  timeout={1000}
-                  classNames="fade"
-                  key={index}
-                >
-                <li key={index}>{item}</li>
-                </CSSTransition>
-              ))
-            }
-          </ul>
+      <div className='container'>
+        <TransitionGroup className='todo-list'>
+          {this.state.items.map((item, i) => (
+            <Fade key={item}>
+              <div>
+                {`${item} `}
+                <button onClick={() => this.handleRemove(i)}>
+                  &times;
+                </button>
+              </div>
+            </Fade>
+          ))}
         </TransitionGroup>
+        <button onClick={() => this.handleAdd()}>Add Item</button>
       </div>
-    )
+    );
   }
 }
