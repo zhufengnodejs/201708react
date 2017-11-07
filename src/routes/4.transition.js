89d96d25229e4d6ab622d10a97b79e1016f2b8a3
zhufengnodejs <zhufengnodejs@126.com>
2017-11-07 17:48:26 +0800

    react面试题

--- src/routes/4.transition.js
+++ src/routes/4.transition.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {CSSTransition} from 'react-transition-group';
+import './4.transition.css'
 /**
  * 在一个组件进入或离开的时候添加一系列的类名
  * 比如在进行的时候 example-enter example-enter-active
@@ -12,9 +13,9 @@ export default class Todos extends Component {
 
   addTodo = (event) => {
     let keyCode = event.keyCode;
-    if(keyCode == 13){
+    if (keyCode == 13) {
       let text = event.target.value;
-      this.setState({todos:[...this.state.todos,text]});
+      this.setState({todos: [...this.state.todos, text]});
       event.target.value = '';
     }
   }
@@ -25,13 +26,19 @@ export default class Todos extends Component {
         <input
           onKeyDown={this.addTodo}
           type="text"/><br/>
-        <ul>
-          {
-            this.state.todos.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))
-          }
-        </ul>
+
+        <CSSTransition
+          classNames="fade"
+          timeout={1000}
+        >
+          <ul>
+            {
+              this.state.todos.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))
+            }
+          </ul>
+        </CSSTransition>
       </div>
     )
   }
