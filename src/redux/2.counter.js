b53c57c246a48e555d7179705a48791953e6e67f
zhangrenyang <zhang_renyang@126.com>
2017-11-08 17:46:46 +0800

    commit

--- src/redux/2.counter.js
+++ src/redux/2.counter.js
@@ -3,37 +3,42 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 const ADD = 'ADD';
 const SUB = 'SUB';
-let reducer = (state=0,action)=>{
+let initState = {number:0};
+let reducer = (state=initState,action)=>{
     switch(action.type){
         case ADD:
-            return state + 1;
+            return {number:state.number+action.payload};
         case SUB:
-            return state - 1;
+            return {number:state.number-action.payload};
         default:
             return state;
     }
 }
 let store = createStore(reducer);
 
-
 class Counter extends Component{
     constructor(){
         super();
         //在初始化的时候，把仓库中的状态拿过来存放到自己的内部状态中
-        this.state = {number:store.getState()};
+        this.state = {number:store.getState().number};
     }
     //在组件渲染完成后订阅仓库的状态变化事件
     componentDidMount(){
-      store.subscribe(()=>{
-         this.setState({number:store.getState()});
+      //取消订阅
+      this.unSubscribe = store.subscribe(()=>{
+         this.setState({number:store.getState().number});
       })
+    }//在组件卸载的时候需要取消订阅
+    componentWillUnmount(){
+        this.unSubscribe();
     }
     render(){
         return (
             <div>
                 <p>{this.state.number}</p>
-                <button onClick={()=>store.dispatch({type:ADD})}>+</button>
-                <button onClick={()=>store.dispatch({type:SUB})}>-</button>
+                <input type="text" ref={input=>this.input = input}/>
+                <button onClick={()=>store.dispatch({type:ADD,payload:parseFloat(this.input.value)})}>+</button>
+                <button onClick={()=>store.dispatch({type:SUB,payload:parseFloat(this.input.value)})}>-</button>
             </div>
         )
     }
