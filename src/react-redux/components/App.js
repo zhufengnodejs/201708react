55c37624411789e29aeffce3feb93628cfc3087d
zhangrenyang <zhang_renyang@126.com>
2017-11-09 17:35:37 +0800

    周四下午

---
+++ src/react-redux/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from "./Counter";
+import Todos from "./Todos";
+export default class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <Counter/>
+                <Todos/>
+            </div>
+        )
+    }
+}
